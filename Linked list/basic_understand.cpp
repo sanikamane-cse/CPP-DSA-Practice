312bbea8481b34b4106128fa298d9508b60a5bf1
sanikamane-cse <msanika487@gmail.com>
2026-08-14 11:36:41 +0530

    Array program

---
+++ Linked list/basic_understand.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+};
+int main(){
+    Node* head=new Node();
+    Node* second=new Node();
+    Node* third=new Node();
+
+
+    head->data=10;
+    second->data=20;
+    third->data=30;
+
+    head->next=second;
+    second->next=third;
+    third->next=NULL;
+
+
+    Node* temp=head;
+
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    return 0;
+}
