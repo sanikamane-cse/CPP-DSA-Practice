535576044589814d3b60e633016b58fdf7ece53d
sanikamane-cse <msanika487@gmail.com>
2026-08-15 08:53:33 +0530

    Linked list programs

--- Linked list/insertnodeatbegining.cpp
+++ Linked list/insertnodeatbegining.cpp
@@ -1,28 +1,35 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Node{
-    public:
+
+class Node {
+public:
     int data;
     Node* next;
-};
-int main(){
-    Node* head=new Node();
-    head->data=10;
-    head->next=NULL;
 
-    Node* newNode=new Node();
-    newNode->data=5;
-    newNode->next=head;
-
-    head=newNode;
-
-    Node* temp=head;
-
-    while(temp !=NULL){
-        cout<<temp->data<<" ";
-        temp=temp->next;
+    Node(int val) {
+        data = val;
+        next = NULL;
     }
+};
+
+int main() {
+
+    Node* head = new Node(10);
+    Node* second = new Node(20);
+
+    head->next = second;
+
+    Node* newNode = new Node(5);
+
+    newNode->next = head;
+    head = newNode;
+
+    Node* temp = head;
+
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
     return 0;
-
-
 }
---
+++ Linked list/reverse_linkedlist_two.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+int main(){
+ Node* head=new Node(1);
+Node* second=new Node(2);
+Node* third=new Node(3);
+Node* forth=new Node(4);
+Node* fifth=new Node(5);
+
+head->next=second;
+second->next=third;
+third->next=forth;
+forth->next=fifth;
+
+int left=2;
+int right=4;
+
+Node dummy(0);
+dummy.next=head;
+
+Node* prev=&dummy;
+for(int i=1;i<left;i++){
+    prev=prev->next;
+}
+
+Node* curr=prev->next;
+for(int i=0;i<right-left;i++){
+    Node* next=curr->next;
+    curr->next=next->next;
+    next->next=prev->next;
+    prev->next=next;
+}
+Node* temp = dummy.next;
+
+while(temp != NULL){
+    cout << temp->data << " ";
+    temp = temp->next;
+}
+
+return 0;
+
+
+}
+
