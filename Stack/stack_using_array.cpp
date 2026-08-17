2549ad51a1e7ba0f48815fe07fc1029ec7ac7caf
sanikamane-cse <msanika487@gmail.com>
2026-08-17 08:18:49 +0530

    Stack programs

---
+++ Stack/stack_using_array.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int stack[5];
+int top=-1;
+
+void push(int value){
+    if(top==4){
+        cout<<"Stack is full";
+    }else{
+        top++;
+        stack[top]=value;
+    }
+}
+
+void pop(){
+    if(top==-1){
+        cout<<"Stack is empty";
+    }else{
+        cout<<"Popped:"<<stack[top]<<endl;
+        top--;
+    }
+}
+
+void peek(){
+    if(top==-1){
+        cout<<"Stack is empty";
+    }else{
+        cout<<"Top:"<<stack[top]<<endl;
+    }
+}
+int main(){
+    push(10);
+    push(20);
+    push(30);
+
+    peek();
+    pop();
+    peek();
+
+    return 0;
+}
