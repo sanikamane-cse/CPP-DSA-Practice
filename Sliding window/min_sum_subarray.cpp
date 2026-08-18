1aba8928f13ce0fcf465fc597dc6c1bbe662216d
Sanika Mane <msanika487@gmail.com>
2026-08-18 15:17:47 +0530

    sliding window program

--- Sliding window/min_sum_subarray.cpp
+++ Sliding window/min_sum_subarray.cpp
@@ -1,14 +1,26 @@
 #include<iostream>
 using namespace std;
-
 int main(){
-    
-}
 
-int arr[7]={2,5,1,8,2,9,1};
-int k=3;
-int min_sum=0;
+    int arr[7]={2,3,1,5,4,3,2};
+    int k=3;
+    int wind_sum=0;
 
-for(int i=0;i<k;i++){
+    for(int i=0;i<k;i++){
+       wind_sum+=arr[i];
+    }
 
+    int min_sum=wind_sum;
+
+    for(int i=k;i<7;i++){
+        wind_sum=wind_sum-arr[i-k]+arr[i];
+
+        if(wind_sum<min_sum){
+            min_sum=wind_sum;
+        }
+
+    }
+
+    cout<<"Minimum sum:"<<min_sum;
+    return 0;
 }
