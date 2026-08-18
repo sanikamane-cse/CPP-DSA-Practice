f09b1c0b7113db51ae7e294b53d3687097a04f2d
sanikamane-cse <msanika487@gmail.com>
2026-08-18 15:08:54 +0530

    Stack programs

---
+++ Sliding window/min_sum_subarray.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+}
+
+int arr[7]={2,5,1,8,2,9,1};
+int k=3;
+int min_sum=0;
+
+for(int i=0;i<k;i++){
+
+}
---
+++ Stack/reverse_string.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+
+    string str="hello";
+    stack<char>s;
+
+    for(int i=0;i<str.length();i++){
+        s.push(str[i]);
+    }
+    
+    while(!s.empty()){
+        cout<<s.top();
+         s.pop();
+      }
+       
+      return 0;
+
+
+}
