3031eda1deac92e4c332b2d6b7a4c2d08a34e79f
sanikamane-cse <msanika487@gmail.com>
2026-08-22 22:40:24 +0530

    array problems

---
+++ Array/sum_of_column.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[3][3]=
+    {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+
+    for(int j=0;j<3;j++){
+
+        int sum=0;
+
+        for(int i=0;i<3;i++){
+            sum=sum+arr[i][j];
+        }
+
+        cout<<"Sum of column "<< j + 1<<" = "<<sum<<endl;
+
+    }
+    return 0;
+}
