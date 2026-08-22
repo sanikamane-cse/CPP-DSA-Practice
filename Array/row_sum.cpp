c70a43c57af0af022ec7c050716518a7bb6b8687
sanikamane-cse <msanika487@gmail.com>
2026-08-22 22:34:36 +0530

    Sum of row solution

---
+++ Array/row_sum.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[3][3]=
+    {
+      {1,2,3},
+      {4,5,6},
+      {7,8,9}
+    };
+
+    for(int i=0;i<3;i++){
+
+        int sum=0;
+
+        for(int j=0;j<3;j++){
+            sum=sum+arr[i][j];
+
+        }
+        cout<<"Sum of row "<<i+1<<" = "<<sum<<endl;
+    }
+
+    return 0;
+}
