4b904487688a8b7759769c86e6b4835141920ce3
sanikamane-cse <msanika487@gmail.com>
2026-08-30 22:54:39 +0530

    String  problems

---
+++ string/first_unique_char_in_string.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+
+int main(){
+string s="leetcode";
+vector<int> freq(26,0);
+
+for(int i=0;i<s.size();i++){
+     freq[s[i]-'a']++;
+      
+}
+
+for(int i=0;i<s.size();i++){
+    if(freq[s[i]-'a']==1){
+        cout<<s[i]<<endl;
+        break;
+    }
+}
+return 0;
+}
+
+
