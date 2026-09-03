8c39e9ba19f95c1080ba605dd72c6a2dd9b92169
sanikamane-cse <msanika487@gmail.com>
2026-09-03 10:36:00 +0530

    String programs

---
+++ string/count_consonant_vowels.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+void count(string s, int &vowelCount, int &consonantCount) {
+
+    for (int i = 0; i < s.length(); i++) {
+
+        char ch = tolower(s[i]);
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || 
+            ch == 'o' || ch == 'u') {
+            vowelCount++;
+        }
+        else if (ch >= 'a' && ch <= 'z') {
+            consonantCount++;
+        }
+    }
+}
+
+int main() {
+
+    string s = "I Love Coding";
+
+    int vowelCount = 0;
+    int consonantCount = 0;
+
+    count(s, vowelCount, consonantCount);
+
+    cout << "Vowel: " << vowelCount << endl;
+    cout << "Consonant: " << consonantCount << endl;
+
+    return 0;
+}
